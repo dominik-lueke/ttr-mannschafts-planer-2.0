520ba5964c85169d496cc4762f71326aaca8c85c
Dominik Lueke <dominik.lueke@gmail.com>
2019-12-26 12:43:40 +0100

    refactor spielerView html markup

--- src/js/view/spielerView.js
+++ src/js/view/spielerView.js
@@ -5,25 +5,25 @@ class SpielerView {
       this.spieler = spieler
       const ttrdifferenz = `${spieler.mannschaft}.${spieler.position}` != "1.1" ? spieler.ttrdifferenz : "";
       const ttrdifferenzVorzeichen = ttrdifferenz > 0 ? "+" : ""
-      this.html = $(`
-        <li id="spieler-${spieler.spielklasse}-${spieler.id}" class="list-group-item spieler">
-          <div class="d-flex">
-            <div id="spieler-${spieler.spielklasse}-${spieler.id}-position" class="p-2 text-muted">${spieler.mannschaft}.${spieler.position}</div>
-            <div id="spieler-${spieler.spielklasse}-${spieler.id}-name" class="p-2 flex-grow-1 link spieler-name">${spieler.name}</div>
-            <div id="spieler-${spieler.spielklasse}-${spieler.id}-spv" class="p-2 spv"><span class="badge badge-danger spv-badge">SPV</span></div>
-            <div id="spieler-${spieler.spielklasse}-${spieler.id}-qttr" class="p-2 ttr-wert text-muted">${spieler.qttr}</div>
-            <div id="spieler-${spieler.spielklasse}-${spieler.id}-ttrdifferenz" class="p-2 ttr-difference">${ttrdifferenzVorzeichen}${ttrdifferenz}</div>
-            <div id="spieler-${spieler.spielklasse}-${spieler.id}-invalid-icon" class="p-2 spieler-invalid-icon text-danger"><span><i class="fa fa-exclamation-triangle"></i></span></div>
-          </div>
-        </li>
-      `)
-      spielerListeContainer.append(this.html)
+
+      // Create the HTML Markup
+      this.spieler_div = $(`<li id="spieler-${spieler.spielklasse}-${spieler.id}" class="list-group-item spieler"></li>`)
+      this.spieler_flex_div = $(`<div class="d-flex"></div>`)
+      this.spieler_flex_div.append( this.spieler_position_div = $(`<div id="spieler-${spieler.spielklasse}-${spieler.id}-position" class="p-2 text-muted">${spieler.mannschaft}.${spieler.position}</div>`) )
+      this.spieler_flex_div.append( this.spieler_name_div = $(`<div id="spieler-${spieler.spielklasse}-${spieler.id}-name" class="p-2 flex-grow-1 link spieler-name">${spieler.name}</div>`) )
+      this.spieler_flex_div.append( this.spieler_spv_div = $(`<div id="spieler-${spieler.spielklasse}-${spieler.id}-spv" class="p-2 spv"></div>`) )
+      this.spieler_spv_div.append( this.spieler_spv_badge = $(`<span class="badge badge-danger spv-badge">SPV</span>`) )
+      this.spieler_flex_div.append( this.spieler_qttr_div = $(`<div id="spieler-${spieler.spielklasse}-${spieler.id}-qttr" class="p-2 ttr-wert text-muted">${spieler.qttr}</div>`) )
+      this.spieler_flex_div.append( this.spieler_ttrdifferenz_div = $(`<div id="spieler-${spieler.spielklasse}-${spieler.id}-ttrdifferenz" class="p-2 ttr-difference">${ttrdifferenzVorzeichen}${ttrdifferenz}</div>`) )
+      this.spieler_flex_div.append( this.spieler_qttr_input_div = $(`<div class="p-2 ttr-wert text-muted display-none"></div>`) )
+      this.spieler_qttr_input_div.append( this.spieler_qttr_input = $(`<input id="spieler-${spieler.spielklasse}-${spieler.id}-qttr-input" type="number" class="form-control" min="0" max="3000" value="${spieler.qttr}"></input>`) )
+      this.spieler_flex_div.append( this.spieler_invalid_icon = $(`<div id="spieler-${spieler.spielklasse}-${spieler.id}-invalid-icon" class="p-2 spieler-invalid-icon text-danger"><span><i class="fa fa-exclamation-triangle"></i></span></div>`) )
+      this.spieler_div.append(this.spieler_flex_div)
+      spielerListeContainer.append(this.spieler_div)
+
       // color the ttr-differenz
       $(`#spieler-${spieler.spielklasse}-${spieler.id}-ttrdifferenz`).addClass(ttrdifferenz < 0 ? "text-success" : ttrdifferenz > 0 ? "text-danger" : "")
-      // cache jq elements
-      this.spieler_div = $(`#spieler-${spieler.spielklasse}-${spieler.id}`)
-      this.spieler_invalid_icon = $(`#spieler-${spieler.spielklasse}-${spieler.id}-invalid-icon`)
-      this.spv_badge = $(`#spieler-${spieler.spielklasse}-${spieler.id}-spv span`)
+
       // add invalid class, invalid-icon, tooltip
       if (spieler.invalid && spieler.invalid.length > 0) {
         this.spieler_div.addClass("invalid")
@@ -51,14 +51,14 @@ class SpielerView {
 
       // add spv-badge
       if ( ! spieler.spv ){
-        this.spv_badge.removeClass("badge-danger")
-        this.spv_badge.addClass("badge-light")
+        this.spieler_spv_badge.removeClass("badge-danger")
+        this.spieler_spv_badge.addClass("badge-light")
       }
       if (spieler.invalid && spieler.invalid.some(invalid_spieler => invalid_spieler.mannschaft != spieler.mannschaft)) {
         this.spieler_div.addClass("spv-possible")
       }
       // add extra hover to spv-badge to hightlight the spieler which would also recieve a spv on higher positionen in the same mannschaft
-      this.spv_badge.hover(
+      this.spieler_spv_badge.hover(
         () => { this._addHighlightsToSpvSpieler() },
         () => { this._removeHighlightsFromSpvSpieler() }
       )
@@ -79,8 +79,8 @@ class SpielerView {
 
   _addHighlightsToSpvSpieler(){
     // toggle color on our own spv-badge
-    this.spv_badge.addClass("badge-danger")
-    this.spv_badge.removeClass("badge-light")
+    this.spieler_spv_badge.addClass("badge-danger")
+    this.spieler_spv_badge.removeClass("badge-light")
     // display spv-bage on other spieler with higher positions in same mannschaft
     const prev_spieler = this.spieler_div.prevAll()
     prev_spieler.addClass("spv-highlight")
@@ -91,8 +91,8 @@ class SpielerView {
 
   _removeHighlightsFromSpvSpieler(){
     // toggle color on our own spv-badge
-    this.spv_badge.removeClass("badge-danger")
-    this.spv_badge.addClass("badge-light")
+    this.spieler_spv_badge.removeClass("badge-danger")
+    this.spieler_spv_badge.addClass("badge-light")
     // hide spv-bage on other spieler with higher positions in same mannschaft
     const prev_spieler = this.spieler_div.prevAll()
     prev_spieler.removeClass("spv-highlight")
@@ -102,7 +102,7 @@ class SpielerView {
   }
 
   delete() {
-    this.html.remove()
+    this.spieler_div.remove()
   }
 
 }

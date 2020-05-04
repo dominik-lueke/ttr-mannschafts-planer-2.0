190ed33da560e1568f281b161426e86395e41339
Dominik Lüke <dominik.lueke@gmail.com>
2020-05-04 10:08:37 +0200

    start working on version 2.1.0

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 set name=TischtennisMannschaftsPlaner
 set platform=win32
 set arch=x64
-set version=2.0.0
+set version=2.1.0
 
 build\package.bat %name% %platform% %arch% %version% && build\archive.bat %name% %platform% %arch% %version%

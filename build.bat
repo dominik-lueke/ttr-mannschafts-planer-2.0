b97d9edab522fd7de34dda19ba46138c6b204317
Dominik Lueke <dominik.lueke@gmail.com>
2020-05-19 16:55:50 +0200

    release version to 2.1.1

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 set name=TischtennisMannschaftsPlaner
 set platform=win32
 set arch=x64
-set version=2.2-SNAPSHOT
+set version=2.1.1
 
 build\package.bat %name% %platform% %arch% %version% && build\archive.bat %name% %platform% %arch% %version%
